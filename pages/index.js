87bf546b564862333e3b78587ec8e41043b0db42
LuisanaHenmary <luisanahenmaryp@gmail.com>
2022-10-05 10:48:15 -0400

    curr

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
+import { useCallback, useEffect, useState} from "react"
+
 const Home = ({data}) => {
+
+  const [mout, setMout] = useState(0)
+  const symbols = data.symbols
+
+  const currencyConversion = useCallback(async () => {
+    const query = "USD_PHP";
+
+    try {
+      const resp = await fetch(`https://free.currconv.com/api/v7/convert?q=${query}&compact=ultra&apiKey=${data.key_curr}`)
+      const info = await resp.json()
+      setMout(info[query])
+
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  useEffect(() => {
+    currencyConversion()
+  }, [currencyConversion]);
+
+
   return (
     <div>
-      test
-      {Object.keys(data).map((key,index)=> <div key={index} >{`${data[key]} (${key})`}</div> )}
+      {mout}
+      {Object.keys(symbols).map((key,index)=> <div key={index} >{`${symbols[key]} (${key})`}</div> )}
     </div>
   )
 }
@@ -11,7 +35,12 @@ export async function getStaticProps() {
   try {
       const responce = await fetch(`http://data.fixer.io/api/symbols?access_key=${process.env.FIXER_KEY}`)
       const info = await responce.json()
-      const data = info.symbols
+
+      const data = {
+        symbols: info.symbols,
+        key_curr: process.env.CURR_CONV
+      }
+
       return {
           props: {
               data
